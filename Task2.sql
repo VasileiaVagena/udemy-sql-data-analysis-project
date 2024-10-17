b73126d9bb35b9200946ed97bb81f4956f00ea7c
Vasileia Vagena <110908916+VasileiaVagena@users.noreply.github.com>
2024-10-17 12:16:32 +0200

    Update Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -1,5 +1,6 @@
--- Task2: Compare the number of male managers to the number of female managers from different departments for each year, 
--- starting from 1990.
+/* Task2: Compare the number of male managers to the number of female managers from different departments for each year, 
+    starting from 1990. 
+*/
 
 
 SELECT 
