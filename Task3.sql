990fb819234cbdecdf494b804ebfc220e5f7b66b
Vasileia Vagena <110908916+VasileiaVagena@users.noreply.github.com>
2024-10-17 12:20:10 +0200

    Update Task3.sql

--- Task3.sql
+++ Task3.sql
@@ -1,4 +1,10 @@
--- Compare the average salary of female versus male employees in the entire company
+/*
+Compare the average salary of female versus male employees in the entire company 
+until year 2002, and add a filter allowing you to see that per each department.
+*/
+
+
+
 
 SELECT 
     e.gender,
