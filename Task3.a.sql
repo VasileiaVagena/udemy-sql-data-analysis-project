c5ab17d4a63bd546139168a453a4948304c5d36f
Vasileia Vagena <110908916+VasileiaVagena@users.noreply.github.com>
2024-10-17 12:18:44 +0200

    Update Task3.a.sql

--- Task3.a.sql
+++ Task3.a.sql
@@ -1,9 +1,14 @@
--- Apply the column names suggested. In order from left to right, they refer to the employees, departments, salaries, and dept_emp tables, respectively. 
--- Please note that the third column contains only average salary values rounded to the nearest cent. The last column should contain 'before' or 'on or after' 
--- depending on whether the employee's start date (stored in the dept_emp's from_date column) is before or on or after January 1, 1998.
+/*
+Apply the column names suggested. In order from left to right, they refer to the employees, departments, salaries, 
+and dept_emp tables, respectively. 
+Please note that the third column contains only average salary values rounded to the nearest cent. The last column should 
+contain 'before' or 'on or after' depending on whether the employee's start date (stored in the dept_emp's from_date column)
+is before or on or after January 1, 1998.
 
--- Group by the department number dept_no, employee's gender, and jan_1_1998 columns. Retrieve only data about contracts signed in 1990 or later. 
--- Sort the obtained results by department number in ascending order.
+Group by the department number dept_no, employee's gender, and jan_1_1998 columns. Retrieve only data about contracts signed
+in 1990 or later. 
+Sort the obtained results by department number in ascending order.
+*/
 
 
 SELECT 
